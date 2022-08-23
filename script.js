9acc8b8aa581dd11d971f1d8618edd8aa065122a
Andrey Dmitriev <admit8490@gmail.com>
2022-08-23 20:54:25 +0200

    ading tasks dynamically plus additional styles

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 // constants
 
+const taskContainer = document.getElementById('task-container');
+const inputTask = document.getElementById('input-task');
+const addTask = document.getElementById('add-task');
+
+// console.log(addTaskcontainer, inputTask, addTask);
+
 // functions
 
+const addNewTask = () => {
+  let task = document.createElement('div');
+  task.classList.add('task');
+
+  let li = document.createElement('li');
+  li.innerText = `${inputTask.value}`;
+  task.appendChild(li);
+  
+  let checkButton = document.createElement('button');
+  checkButton.innerHTML
+    = '<i class="fa-solid fa-check"></i>';
+  checkButton.classList.add('checkTask');
+  task.appendChild(checkButton);
+
+  let deleteButton = document.createElement('button');
+  deleteButton.innerHTML
+    = '<i class="fa-solid fa-trash-can"></i>';
+  deleteButton.classList.add('deleteTask');
+  task.appendChild(deleteButton);
+
+  if (inputTask.value === '') {
+    alert('Hey, write something!');
+  }
+  else {
+    taskContainer.appendChild(task);
+  }
+
+  inputTask.value = "";
+}
+
 // event listeners
+
+addTask.addEventListener('click', addNewTask);
